5ad0a7785054137a3744951ed2ca8b78e146ff3e
dantran304 <dantrandinhh@gmail.com>
2016-06-13 00:34:03 +0700

    load image

--- Assets/Script/HUDManager.cs
+++ Assets/Script/HUDManager.cs
@@ -6,9 +6,21 @@ public class HUDManager : MonoBehaviour
 {
 
 	public string bundleURL;
-	public string assetName;
+	public string assetName_Image;
 
-	IEnumerator Start ()
+	public GameObject sprite2D;
+
+	Sprite _image;
+
+
+	// Load Image
+	public void LoadImage ()
+	{
+		StartCoroutine (iLoadImage ());
+	}
+
+
+	IEnumerator iLoadImage ()
 	{
 		// Download the file from the URL. It will not be saved in the Cache
 		using (WWW www = new WWW (bundleURL)) {
@@ -16,10 +28,22 @@ public class HUDManager : MonoBehaviour
 			if (www.error != null)
 				throw new Exception ("WWW download had an error:" + www.error);
 			AssetBundle bundle = www.assetBundle;
-			if (assetName == "")
-				Instantiate (bundle.mainAsset);
-			else
-				Instantiate (bundle.LoadAsset (assetName));
+
+//			if (assetName_Image == "")
+//				Instantiate (bundle.mainAsset);
+//			else
+//				Instantiate (bundle.LoadAsset (assetName_Image));
+
+			//lấy ảnh từ bundle
+			Sprite im = bundle.LoadAsset<Sprite> (assetName_Image);
+
+			Debug.Log (im);
+
+			_image = Instantiate (im) as Sprite;
+
+			//load vào đối tượng
+			sprite2D.GetComponent<SpriteRenderer> ().sprite = _image;
+
 			// Unload the AssetBundles compressed contents to conserve memory
 			bundle.Unload (false);
 
